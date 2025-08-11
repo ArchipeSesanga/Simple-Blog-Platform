e8dabe0bdb88b7f07b4dc5f0542a55fd5caa20c9
Archipe Sesanga <moisemozess@gmail.com>
2025-08-11 09:01:59 +0200

    Alpha release

---
+++ index.js
@@ -0,0 +1,39 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+//setting the engine
+app.set("view engine", "ejs");
+
+//set the middleware
+
+app.use(express.urlencoded({ extended: true })); // to be able to read data sent in the form
+app.use(express.static("public")) // to find static items 
+
+
+//in Memory "Database"
+let recipes =[];
+
+
+ app.get("/",  (req, res) =>{
+    res.render("index", {recipes});
+ });
+
+ app.get("/new", (req, res) =>{
+    res.render("new");
+ ;});
+
+app.post("/new", (req, res) => {
+    const { title, ingredients, instructions, author } = req.body; 
+
+    recipes.push({ title, ingredients, instructions, author });
+
+    // Render the post page with the submitted data
+    res.redirect("/");
+});
+
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
